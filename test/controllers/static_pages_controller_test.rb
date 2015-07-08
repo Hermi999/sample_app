e3ee0e32d2245b4cfeaf9f996c3e9c785ecc1fa9
Hermann Wagner <hermann.wagner01@gmx.at>
2015-07-08 02:54:11 +0200

    Eliminate repetition (solves exercise 3.1)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,21 +3,27 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  # The setup function is automatically run bevore every test
+  def setup
+  	@base_title = "Ruby on Rails Tutorial Sample App"
+  end
+
+
   test "should get home" do
     get :home										# Issue a get request to the 'home' action of the StaticPages Controller
     assert_response :success		# The response should be the HTTP Status Code '200 ok'
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"		# Check for a tag named "title" with assert_select witch contains the string "..."
+    assert_select "title", "Home | #{@base_title}"		# Check for a tag named "title" with assert_select witch contains the string "..."
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{@base_title}"
   end
 
   test "should get about" do
   	get :about
   	assert_response :success
-  	assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  	assert_select "title", "About | #{@base_title}"
   end
 end
