8814f93ddb30514084e0c8d2cfbf7cd6b6c16e09
Hermann Wagner <hermann.wagner01@gmx.at>
2015-07-08 05:51:49 +0200

    Introduced a custom helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,23 +1,26 @@
-# run all test with "rake test"
+# run all test with 'rake test'
 
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-  test "should get home" do
-    get :home										# Issue a get request to the 'home' action of the StaticPages Controller
-    assert_response :success		# The response should be the HTTP Status Code '200 ok'
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"		# Check for a tag named "title" with assert_select witch contains the string "..."
+  test 'should get home' do
+    # Issue a get request to the 'home' action of the StaticPages Controller
+    get :home
+    # The response should be the HTTP Status Code '200 ok'
+    assert_response :success
+    # Check for a tag named 'title' with witch contains the string '...'
+    assert_select 'title', 'Ruby on Rails Tutorial Sample App'
   end
 
-  test "should get help" do
+  test 'should get help' do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select 'title', 'Help | Ruby on Rails Tutorial Sample App'
   end
 
-  test "should get about" do
-  	get :about
-  	assert_response :success
-  	assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+  test 'should get about' do
+    get :about
+    assert_response :success
+    assert_select 'title', 'About | Ruby on Rails Tutorial Sample App'
   end
 end
