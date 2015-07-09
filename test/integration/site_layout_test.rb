9c1a965d7f96e91ba792ae66db80ba2ba9d63c84
Hermann Wagner <hermann.wagner01@gmx.at>
2015-07-09 22:45:46 +0200

    finish exercises

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -12,4 +12,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select 'a[href=?]', contact_path
     assert_select 'a[href=?]', signup_path
   end
+
+  test 'page title' do
+    get signup_path
+    assert_select 'title', full_title('Sign up')
+  end
 end
