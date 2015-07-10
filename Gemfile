c2e8f8a0a34f028bea3b340ba422a80ccee0717a
Hermann Wagner <hermann.wagner01@gmx.at>
2015-07-10 10:06:51 +0200

    Added user password and validation

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', 		'4.2.2'
+# Hash function for passwords
+gem 'bcrypt',   '3.1.7'
 # Convert bootstrap less files to sass
 gem 'bootstrap-sass',   '3.2.0.0'
 # Use SCSS for stylesheets
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,16 @@
 class User < ActiveRecord::Base
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i    # constant
   # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+[.][a-z]+\z/i
+
+  before_save { self.email = email.downcase }   # self keyword is optional
+                                                # on right-hand side
+
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true,
                     length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
+
+  has_secure_password
+  validates :password, presence: true, length: { minimum: 6 }
 end
