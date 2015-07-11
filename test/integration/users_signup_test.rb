a351433b313b0152ddad083faf1d1ddf8856159e
Hermann Wagner <hermann.wagner01@gmx.at>
2015-07-11 19:22:57 +0200

    Added integration test for successful user signup & redircet

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  test 'a user with invalid signup information does not get saved' do
+  test 'a user with invalid signup info does not get saved' do
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, user: {
@@ -12,5 +12,19 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                              }
     end
     assert_template 'users/new'   # does fail leed to new-page again?
+  end
+
+
+  test ' a user with valid signup info gets saved in database' do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user:  {
+                                name:                 'Hermann',
+                                email:                'a@a.at',
+                                password:             'dasisteintest',
+                                password_confirmation:'dasisteintest'
+                              }
+    end
+    assert_template 'users/show'
   end
 end
