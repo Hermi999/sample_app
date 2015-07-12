ea58ea6a50090ed86e71396f85d21430f1c93877
Hermann Wagner <hermann.wagner01@gmx.at>
2015-07-12 10:02:15 +0200

    exercise 2

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  test 'a user with invalid signup info does not get saved' do
+  test 'a user with invalid signup info does not get saved & shows errors' do
     get signup_path
     assert_no_difference 'User.count' do
       post users_path, user: {
@@ -12,7 +12,86 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                              }
     end
     assert_template 'users/new'   # does fail leed to new-page again?
+    # errors?
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
+
+
+
+  test 'invalid signup infos leed to error messages' do
+    get signup_path
+
+    # create different invalid users
+    empty_user =
+                { user:
+                  { name: '',
+                    email: '',
+                    password: '',
+                    password_confirmation: ''
+                  }
+                }
+    empty_email_user =
+                { user:
+                   { name:                   'hermann',
+                     email:                  'abc',
+                     password:               'dasisteinpassword',
+                     password_confirmation:  'dasisteinpassword'
+                   }
+                }
+    short_password_user =
+                { user:
+                   { name:                   'hermann',
+                     email:                  'hermann.wagner01@gmx.at',
+                     password:               'abc',
+                     password_confirmation:  'abc'
+                   }
+                }
+    password_conf_user =
+                { user:
+                   { name:                   'hermann',
+                     email:                  'hermann.wagner01@gmx.at',
+                     password:               'dasisteinpassword',
+                     password_confirmation:  'dasisteinpassworD'
+                   }
+                }
+    valid_user =
+                { user:
+                   { name:                   'hermann',
+                     email:                  'hermann.wagner01@gmx.at',
+                     password:               'dasisteinpassword',
+                     password_confirmation:  'dasisteinpassword'
+                   }
+                }
+
+    # test all empty messages
+    post users_path, empty_user
+    assert_select 'li', "Name can't be blank"
+    assert_select 'li', "Email can't be blank"
+    assert_select 'li', "Password can't be blank"
+
+    # test invalid email message
+    post users_path, empty_email_user
+    assert_select 'li', 'Email is invalid'
+    assert_select 'div', 'The form contains 1 error.'
+
+    # test user password length message
+    post users_path, short_password_user
+    assert_select 'li', 'Password is too short (minimum is 6 characters)'
+    assert_select 'div.alert-danger', 'The form contains 1 error.'
+
+    # test password confirmation
+    post users_path, password_conf_user
+    assert_select 'li', "Password confirmation doesn't match Password"
+    assert_select 'div.alert-danger', 'The form contains 1 error.'
+
+    # test if email has already been taken message is visible
+    post users_path, valid_user
+    post users_path, valid_user
+    assert_select 'li', 'Email has already been taken'
+    assert_select 'div.alert-danger', 'The form contains 1 error.'
+  end
+
 
 
   test ' a user with valid signup info gets saved in database' do
