7541bb15deef4d2aab5f5df048b874ca976824d5
Hermann Wagner <hermann.wagner01@gmx.at>
2015-07-13 13:01:17 +0200

    Handle user-edit post request

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,22 @@
 class UsersController < ApplicationController
+  # Action for showing the the user profile. params[:id] is extracted from
+  # the URL:  '.../users/:id'
   def show
     @user = User.find(params[:id])
   end
 
+  # Action for showing the 'new' view with the signup form. User fills out the
+  # form which then will be submitted to the create action. The data will be
+  # passed via the params hash.
   def new
     @user = User.new
   end
 
+  # Action for creating a new user, based on the data from the signup form in
+  # the 'new' view. The user data is retrieved from the params hash. If it's
+  # not possible to save the user, because the validation in the user model
+  # failed, then re-render the new view. Otherwise jump to the users profile
+  # pate & show a flash message.
   def create
     @user = User.new(user_params)
     if @user.save
@@ -18,15 +28,34 @@ class UsersController < ApplicationController
     end
   end
 
+  # Action for showing the edit view. The formular in the view get's
+  # automatically filled with data from the @user member-variable, because we
+  # are using the form_for Rails method. The user input is submitted per
+  # 'PATCH' (in this case POST, because Browsers can't do PATCH) to the
+  # update-action.
   def edit
-    # Action for editing the user. The formular in the view get's automatically
-    # filled with data from the @user membarvariable, because we are using
-    # the form_for Rails method.
     @user = User.find(params[:id])
   end
 
-  private
+  # Action for updating the user model, based on the data submitted from the
+  # form in the create view. The data is retrieved from the params hash. If it
+  # is not possible to save the changes, because validation in the user model
+  # failed, then re-render the 'edit' view. Otherwise ...
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
 
+  # Private Methods
+  private
+    # Use strong parameters to prevent mass-assignment vulnerabily:
+    # Defination of which hash-keys have to be present in the params hash.
+    # The 'required' one have to be present, while only the 'permited' one's
+    # are get copied into a new hash, which is returned by the method.
     def user_params
       params.require(:user).permit( :name, :email,
                                     :password, :password_confirmation)
