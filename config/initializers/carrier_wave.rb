e408f9bfca2309fcea4a07332369315d3d9c5c87
Hermann Wagner <hermann.wagner01@gmx.at>
2015-07-16 12:59:56 +0200

    changed AWS signatur version again

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,11 +2,11 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       # Configuration for Amazon S3
-      :s3_signature_version  => 'v4',
       :provider              => 'AWS',
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
     config.fog_directory     =  ENV['S3_BUCKET']
+    config.fog_attributes = { :s3_signature_version  => 'v4' }
   end
 end
