91166807b14b2cb740af81ea1debe8e62c7ffecf
Hermann Wagner <hermann.wagner01@gmx.at>
2015-07-18 08:39:15 +0200

    Chapter11-exercises: Added Test

--- test/integration/microposts_interface_test.rb
+++ test/integration/microposts_interface_test.rb
@@ -9,6 +9,7 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get root_path
     assert_select 'div.pagination'
+    assert_select 'input[type=file]'
 
     # Invalid submission
     assert_no_difference 'Micropost.count' do
@@ -18,9 +19,11 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
 
     # Valid submission
     content = 'This is a valid micropost'
+    picture = fixture_file_upload('test/fixtures/rails.png', 'image/png')
     assert_difference 'Micropost.count' do
-      post microposts_path, micropost: { content: content }
+      post microposts_path, micropost: { content: content, picture: picture }
     end
+    assert assigns(:micropost).picture?
     assert_redirected_to root_url
     follow_redirect!
     assert_match content, response.body
@@ -35,5 +38,25 @@ class MicropostsInterfaceTest < ActionDispatch::IntegrationTest
     # Visit a dirfferent user - there shouldn't be a delete link
     get user_path(users(:lini))
     assert_select 'a', text: 'delete', count: 0
+  end
+
+  test 'micropost sidebar count' do
+    log_in_as(@user)
+    get root_path
+    assert_match "#{@user.microposts.count} microposts", response.body
+    delete logout_path
+    follow_redirect!
+    assert_match 'Welcome to the Sample App', response.body
+
+    # User with 0 microposts
+    other_user = users(:mallory)
+    log_in_as(other_user)
+    get root_path
+    assert_match '0 microposts', response.body
+
+    # Create a micropost & check for right singularization
+    other_user.microposts.create!(content: 'Test micropost')
+    get root_path
+    assert_match '1 micropost', response.body
   end
 end
